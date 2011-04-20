bc0c4c6e012b1f73f240b42c6b18d30a13e5abfb
Mateusz Grzechociński <mateusz.grzechocinski@javart.eu>
2011-04-20 14:56:59 +0200

    2.1

---
+++ kickstart-app/src/main/java/pl/javart/android/kickstart/CountActivity.java
@@ -0,0 +1,43 @@
+package pl.javart.android.kickstart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+/**
+ * @author: mgrzechocinski@javart.eu
+ * @since: 20/04/2011
+ */
+public class CountActivity extends Activity {
+
+    protected final String LOG_TAG = CountActivity.class.getSimpleName();
+
+    private int count;
+    private TextView viewById;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            increment();
+            schedule(1000);
+        }
+    };
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.count);
+
+        viewById = (TextView) findViewById(R.id.counter);
+        schedule(0);
+    }
+
+    private void schedule(int delayMillis) {
+        handler.sendEmptyMessageDelayed(0, delayMillis);
+    }
+
+    private void increment() {
+        viewById.setText(Integer.toString(count++));
+    }
+}
