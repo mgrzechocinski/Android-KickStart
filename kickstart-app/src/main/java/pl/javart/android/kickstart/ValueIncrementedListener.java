eff6eb205806cfbf46d41ef2557aac36317571f1
Mateusz Grzechociński <mateusz.grzechocinski@javart.eu>
2011-04-21 10:17:35 +0200

    service with intent brodcasting

---
+++ kickstart-app/src/main/java/pl/javart/android/kickstart/ValueIncrementedListener.java
@@ -0,0 +1,9 @@
+package pl.javart.android.kickstart;
+
+/**
+ * @author: mgrzechocinski@javart.eu
+ * @since: 20/04/2011
+ */
+public interface ValueIncrementedListener {
+    void onValueIncremented(int newValue);
+}
